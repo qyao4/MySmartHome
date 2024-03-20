1c22213492cea9b06e96919a34242ee8b0c909b2
qyao <qyao4@academic.rrc.ca>
2024-03-20 09:39:47 -0500

    filtering onsale new feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,25 @@ class ProductsController < ApplicationController
       @products = @products.where("name LIKE ? or description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
     end
 
+    # Filter by category
     if params[:category].present?
       @products = @products.where(category: params[:category])
+    end
+
+    # Filter products that are on sale
+    if params[:on_sale].present? && params[:on_sale] == 'true'
+      @products = @products.where("discount > 0 AND discount < 1")
+    end
+
+    # Filter new products
+    if params[:new].present? && params[:new] == 'true'
+      @products = @products.where("created_at > ?", 3.days.ago)
+    end
+
+    # Filter recently updated products
+    # Excludes new products from being considered as recently updated
+    if params[:recently_updated].present? && params[:recently_updated] == 'true'
+      @products = @products.where("updated_at > ? AND created_at <= ?", 3.days.ago, 3.days.ago)
     end
 
   end
