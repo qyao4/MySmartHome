ec5585ecd201591fbb7154285e0ba4aaf20d07ba
qyao <qyao4@academic.rrc.ca>
2024-03-20 09:25:47 -0500

    search feature added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,14 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.page(params[:page]).per(6) # 6 products per page
+
+    if params[:search].present?
+      @products = @products.where("name LIKE ? or description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+
+    if params[:category].present?
+      @products = @products.where(category: params[:category])
+    end
+
   end
 end
